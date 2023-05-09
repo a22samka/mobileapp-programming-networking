79927671582b851b3bae2d766ad581f52227fb51
a22samka <a22samka@student.his.se>
2023-05-09 14:19:44 +0200

    test

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
+    private RecyclerView RecyclerView;
     private ArrayList<Mountain> berg;
     private BergAdapter adapter;
 
@@ -23,12 +29,22 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
+
+        RecyclerView = findViewById(R.id.myRecycler_view);
+        berg = new ArrayList<Mountain>();
+        adapter = new BergAdapter(berg);
+        RecyclerView.setAdapter(adapter);
+        RecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
-    }
 
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Mountain>>() {}.getType();
+        ArrayList<Mountain> data = gson.fromJson(json, type);
+        berg.addAll(data);
+        adapter.notifyDataSetChanged();
+    }
 }
